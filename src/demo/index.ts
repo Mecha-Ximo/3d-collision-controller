c8d25151d159e7b2fc0d1a2772fb797bfe66eef3
Joaquin <joaquinbarjor@gmail.com>
2023-09-10 20:47:30 +0200

    feat: change demo

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -21,12 +21,20 @@ camera.position.set(0, 0, 3);
 camera.updateProjectionMatrix();
 
 const sceneRoot = new Scene();
-const cube = createCube(1, 1, 1, 0xddcc22, new Vector3(0, 0, 0));
-const cube2 = createCube(1, 1, 1, 0x66cc22, new Vector3(3, 2, 0));
-const cube3 = createCube(1, 1, 1, 0xdd2222, new Vector3(6, 0, 0));
-const cube4 = createCube(1, 1, 1, 0xccccbb, new Vector3(3, 2, 3));
-const cube5 = createCube(1, 1, 1, 0x11ccdd, new Vector3(3, 2, -3));
-sceneRoot.add(cube, cube2, cube3, cube4, cube5);
+const cube = createCube(1, 4, 1, 0xddcc22, new Vector3(0, 0, 0), new Vector3(0, 0, Math.PI / 3));
+const cube2 = createCube(1, 1, 1, 0x66cc22, new Vector3(3, 2, 0), new Vector3(0, 0, 0));
+const cube3 = createCube(1, 1, 3, 0xdd2222, new Vector3(6, 0, 0), new Vector3(0, Math.PI / 3.2, 0));
+const cube4 = createCube(2, 1, 1, 0xccccbb, new Vector3(3, 2, 3), new Vector3(0, 0, 0));
+const cube5 = createCube(1, 1, 1, 0x11ccdd, new Vector3(3, 2, -3), new Vector3(Math.PI / 3, 0, 0));
+const cube6 = createCube(
+  1,
+  1,
+  1,
+  0x11ccdd,
+  new Vector3(3.01, 0, -3),
+  new Vector3(Math.PI / 3, 0, 0)
+);
+sceneRoot.add(cube, cube2, cube3, cube4, cube5, cube6);
 
 const renderer = new WebGLRenderer({ canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -38,7 +46,7 @@ const controller = new CollisionController(
     highCollisionHeight: 2,
     lowCollisionHeight: 0.2,
     collisionDistance: 1,
-    cameraHeight: 1,
+    cameraHeight: 1.5,
     movementDistance: 0.15,
   },
   sceneRoot,
@@ -77,13 +85,15 @@ function createCube(
   height: number,
   depth: number,
   color: ColorRepresentation,
-  position: Vector3
+  position: Vector3,
+  rotation: Vector3
 ): Mesh {
   const geometry = new BoxGeometry(width, height, depth);
   const material = new MeshBasicMaterial({ color });
 
   const mesh = new Mesh(geometry, material);
   mesh.position.set(position.x, position.y, position.z);
+  mesh.rotation.set(rotation.x, rotation.y, rotation.z);
 
   return mesh;
 }
@@ -107,7 +117,6 @@ window.addEventListener('keydown', (e) => {
 });
 
 window.addEventListener('keyup', (e) => {
-  console.log('KEYUP', e.key);
   if (e.key === 'w') {
     controller.moveForward(false);
   }
